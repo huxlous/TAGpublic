16e574d270dfbbffa350799c44c33edc86dc0775
Joseph <Joseph.Walter001@umb.edu>
2021-06-14 15:56:54 -0400

    framework

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TAGpublic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Read key before closing
+            Console.ReadKey();
+        }
+    }
+}
